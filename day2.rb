6ebc611bea1469c3ec85dbdbb745ffa9bb66a61a
hiroshi0828 <abudieru@gmail.com>
2019-07-13 15:25:17 +0900

    def booleanを追加

--- day2.rb
+++ day2.rb
@@ -6,6 +6,15 @@ def sums
     for i in 1..10 do 
     puts sum += i
  end
-end
+end 
 sums
 
+def boolean(num)
+    if num == 0
+        puts "正しい"
+    elsif
+        num == 1
+        puts "間違い"
+    end
+end
+boolean(0)

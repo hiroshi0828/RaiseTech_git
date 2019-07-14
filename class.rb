0dde285000d404dee6c24297792db3ee6177f595
hiroshi0828 <abudieru@gmail.com>
2019-07-14 10:58:29 +0900

    initializeを追加

--- class.rb
+++ class.rb
@@ -1,11 +1,15 @@
 class Comunication
     attr_accessor :name 
-    
-    
-    def greet
-     puts "#{self.name}こんにちは"
+    attr_accessor :greet
+
+    def initialize(name: ,greet:)
+       self.name = name
+       self.greet = greet
+    end
+    def greeting
+     puts "#{self.name}さん #{self.greet}"
    end
-comunication1 = Comunication.new
-comunication1.name = "hiro"
-comunication1.greet
+comunication1 = Comunication.new(name:"hiro",greet:"hello")
+
+comunication1.greeting
 end
